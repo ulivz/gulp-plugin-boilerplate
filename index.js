abc8444d1d25126d0c3f4c4481257da2acae55d3
ulivz <472590061@qq.com>
2017-07-30 15:22:24 +0800

    Update README and eslint ignore

--- index.js
+++ index.js
@@ -3,34 +3,31 @@ const through = require('through2')
 const PLUGIN_NAME = 'gulp_plugin'
 
 module.exports = function (input, options = {}) {
-
-  // do something
+  // Init
 
   // Create a stream channel to let every file through
   return through.obj(function (file, enc, cb) {
     if (file.isNull()) {
-      cb(null, file);
-      return;
+      cb(null, file)
+      return
     }
 
     if (file.isStream()) {
-      cb(new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-      return;
+      cb(new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'))
+      return
     }
 
     // let file goto next plugin
-    this.push(file);
+    this.push(file)
 
-    cb();
-
+    cb()
   }, function (cb) {
-
     const indexFile = new gutil.File({
       path: __dirname,
-      contents: new Buffer('1994')
+      contents: new Buffer('1994') // eslint-disable-line
     })
 
     this.push(indexFile)
-    cb();
-  });
+    cb()
+  })
 }
